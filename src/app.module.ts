5c35b611a4ce68eb7aa4be20793d86889d29ddbd
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-06-11 01:56:43 +0300

    [EDIT] import clinic, auth and users modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClinicModule } from './domain/clinic/clinic.module';
+import { AuthModule } from './domain/auth/auth.module';
+import { UsersModule } from './domain/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [ClinicModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
