65956dc7efa03b64ff87a313743613ac43135c99
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-06-11 01:50:44 +0300

    [ADD] define main user service

---
+++ src/domain/users/users.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { User } from './interfaces/user.interface';
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [
+    {
+      id: 1,
+      username: 'admin',
+      password: 'admin',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
