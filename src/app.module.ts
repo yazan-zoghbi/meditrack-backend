6f7c4009de4f1b75259998a7e4b1defa5d719946
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-06-11 15:10:47 +0300

    [ADD] import config & jwt modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClinicModule } from './domain/clinic/clinic.module';
 import { AuthModule } from './domain/auth/auth.module';
 import { UsersModule } from './domain/users/users.module';
+import { LocalJwtModule } from './modules/jwt/jwt.module';
 
 @Module({
-  imports: [ClinicModule, AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ClinicModule,
+    AuthModule,
+    UsersModule,
+    LocalJwtModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
