9d007da1c87647f96b5b963802291fdf47a60164
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-06-20 03:28:29 +0300

    [ADD] jwt strategy added

---
+++ src/modules/jwt/jwt.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+
+@Injectable()
+export class JwtServiceStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    return { username: payload.username };
+  }
+}
