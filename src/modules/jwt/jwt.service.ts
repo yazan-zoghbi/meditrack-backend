9aa1e46bc5ab595eb22bedd97b2fa16a516ba90e
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-06-22 00:38:00 +0300

    [FIX] roles guard fixed with jwt strategy

--- src/modules/jwt/jwt.service.ts
+++ src/modules/jwt/jwt.service.ts
@@ -2,9 +2,33 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, ExtractJwt } from 'passport-jwt';
+import { AdminJwtPayload } from 'src/domain/admin/dto/admin-jwt-payload.dto';
+import { Role } from './roles/role.enum';
 
 @Injectable()
-export class JwtServiceStrategy extends PassportStrategy(Strategy) {
+export class AdminJwtServiceStrategy extends PassportStrategy(
+  Strategy,
+  Role.Admin,
+) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: AdminJwtPayload) {
+    if (payload.role !== Role.Admin) {
+      return null;
+    }
+    return payload;
+  }
+}
+@Injectable()
+export class DoctorJwtServiceStrategy extends PassportStrategy(
+  Strategy,
+  Role.Doctor,
+) {
   constructor(configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -13,6 +37,30 @@ export class JwtServiceStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { username: payload.username };
+    return payload;
+  }
+
+  async extractAuthToken(req: any) {
+    return console.log(req.headers.authorization);
+  }
+}
+@Injectable()
+export class PatientJwtServiceStrategy extends PassportStrategy(
+  Strategy,
+  Role.Patient,
+) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    return payload;
+  }
+
+  async extractAuthToken(req: any) {
+    return console.log(req.headers.authorization);
   }
 }
