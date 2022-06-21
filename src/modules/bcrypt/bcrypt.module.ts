2b5b847d0e171d556cf4dd23691d4a5ef1f13985
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-06-22 00:42:30 +0300

    [ADD] bcrypt module with service added

---
+++ src/modules/bcrypt/bcrypt.module.ts
@@ -0,0 +1,9 @@
+import { Module, Global } from '@nestjs/common';
+import { BcryptService } from './bcrypt.service';
+
+@Global()
+@Module({
+  providers: [BcryptService],
+  exports: [BcryptService],
+})
+export class BcryptModule {}
---
+++ src/modules/bcrypt/bcrypt.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class BcryptService {
+  async hashPassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 10);
+  }
+
+  async comparePassword(password: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(password, hash);
+  }
+}
