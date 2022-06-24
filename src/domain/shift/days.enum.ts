72a8709a019f5d04000bf18697cb35f03822746d
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-06-24 13:59:50 +0300

    [ADD] days enum added

---
+++ src/domain/shift/days.enum.ts
@@ -0,0 +1,9 @@
+export enum Day {
+  sunday = 'sunday',
+  monday = 'monday',
+  tuesday = 'tuesday',
+  wednesday = 'wednesday',
+  thursday = 'thursday',
+  friday = 'friday',
+  saturday = 'saturday',
+}
