865b0e4aea84f8caac4091cc81470419a1839c61
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-07-01 01:48:17 +0300

    [FIX] category schema imported

--- src/domain/service/service.module.ts
+++ src/domain/service/service.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Category, CategorySchema } from '../category/category.schema';
 import { ServiceController } from './service.controller';
 import { Service, ServiceSchema } from './service.schema';
 import { ClinicService } from './service.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Service.name, schema: ServiceSchema }]),
+    MongooseModule.forFeature([
+      { name: Service.name, schema: ServiceSchema },
+      { name: Category.name, schema: CategorySchema },
+    ]),
   ],
   providers: [ClinicService],
   controllers: [ServiceController],
