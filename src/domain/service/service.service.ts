1af2804acadc704e92003ce4c4bb53b697a4e7e4
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-07-01 01:50:01 +0300

    [FIX] category populate & parent ref fixed

--- src/domain/service/service.service.ts
+++ src/domain/service/service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Service, ServiceDocument } from './service.schema';
+import { Category, CategoryDocument } from '../category/category.schema';
 import { CreateServiceDto } from './dto/create-service.dto';
 import { UpdateServiceDto } from './dto/update-service.dto';
 
@@ -10,17 +11,25 @@ export class ClinicService {
   constructor(
     @InjectModel(Service.name)
     private readonly serviceModel: Model<ServiceDocument>,
+    @InjectModel(Category.name)
+    private readonly categoryModel: Model<CategoryDocument>,
   ) {}
 
   async getAll(): Promise<ServiceDocument[]> {
-    return this.serviceModel.find();
+    return this.serviceModel.find().populate('category', 'name');
   }
 
   async getByID(id: string): Promise<ServiceDocument> {
     return this.serviceModel.findById(id);
   }
   async create(body: CreateServiceDto): Promise<ServiceDocument> {
-    return this.serviceModel.create(body);
+    const service = await this.serviceModel.create(body);
+    const category = await this.categoryModel
+      .findOne({ _id: body.category })
+      .exec();
+    category.services.push(service);
+    category.save();
+    return service;
   }
 
   async update(id: string, body: UpdateServiceDto): Promise<ServiceDocument> {
