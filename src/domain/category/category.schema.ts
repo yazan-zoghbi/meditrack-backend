8a7e800f7dc97abddc519c56fb219af2028db5fc
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-07-01 01:46:07 +0300

    [FIX] services ref fixed

--- src/domain/category/category.schema.ts
+++ src/domain/category/category.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import mongoose, { Document } from 'mongoose';
+import { Service } from '../service/service.schema';
 
 export type CategoryDocument = Category & Document;
 
@@ -8,8 +9,8 @@ export class Category {
   @Prop({ required: true })
   name: string;
 
-  @Prop({})
-  services: object[];
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Service' }] })
+  services: Service[];
 }
 
 export const CategorySchema = SchemaFactory.createForClass(Category);
