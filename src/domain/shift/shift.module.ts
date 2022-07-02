b1d04499144a291c3b8f72c8d4286ae3ce850a1e
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-07-02 04:17:44 +0300

    [ADD] doctor schema imported

--- src/domain/shift/shift.module.ts
+++ src/domain/shift/shift.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Doctor, DoctorSchema } from '../doctor/schema/doctor.schema';
 import { Shift, ShiftSchema } from './shift.schema';
 import { ShiftService } from './shift.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Shift.name, schema: ShiftSchema }]),
+    MongooseModule.forFeature([
+      { name: Shift.name, schema: ShiftSchema },
+      { name: Doctor.name, schema: DoctorSchema },
+    ]),
   ],
   providers: [ShiftService],
   exports: [ShiftService],
