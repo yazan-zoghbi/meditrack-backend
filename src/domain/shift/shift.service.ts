bc1b810c3f7aa945a0405f97c71801023b30f2c7
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-07-04 03:12:13 +0300

    [ADD] shift limit & time validator added

--- src/domain/shift/shift.service.ts
+++ src/domain/shift/shift.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
 import { Doctor, DoctorDocument } from '../doctor/schema/doctor.schema';
 import { CreateShiftDto } from './dto/create-shift.dto';
 import { UpdateShiftDto } from './dto/update-shift.dto';
@@ -25,20 +25,61 @@ export class ShiftService {
     private readonly doctorModel: Model<DoctorDocument>,
   ) {}
 
-  async create(body: CreateShiftDto) {
-    if (!body.days) {
+  private async dateValidator(days: number[]) {
+    if (!days) {
       throw new Error('should enter valid date');
     }
 
-    const days: string[] = body.days.map((day, i) => {
-      return weekday[body.days[i]];
+    const workingDays = days.map((day, i) => {
+      return weekday[days[i]];
     });
 
+    return workingDays;
+  }
+
+  private async limitValidator(id: ObjectId) {
+    const shiftCount = await this.shiftModel.find({ doctorId: id }).count();
+
+    if (shiftCount >= 2) {
+      throw new Error('Failed! you reach your shifts number limit!');
+    }
+  }
+
+  private async timeValidator(id: ObjectId, start: number, end: number) {
+    if (!start || !end)
+      throw new Error(
+        'make sure to enter valid shift time for starting & end hours',
+      );
+
+    if (start < end) throw new Error('failure in setting a valid shift time');
+
+    const shifts = await this.shiftModel.find({ doctorId: id }).count();
+
+    if (shifts == 1) {
+      const firstShift = await this.shiftModel.findOne({ doctorId: id });
+
+      if (
+        firstShift.start > start ||
+        firstShift.start > end ||
+        firstShift.end > start ||
+        firstShift.end > end
+      ) {
+        throw new Error('failure in setting a valid shift time');
+      }
+    }
+
+    return { start, end };
+  }
+
+  async create(body: CreateShiftDto) {
+    await this.limitValidator(body.doctorID);
+    const workingDays = await this.dateValidator(body.days);
+    const time = await this.timeValidator(body.doctorID, body.start, body.end);
     const shift = await this.shiftModel.create({
       doctorId: body.doctorID,
-      days: days,
-      start: body.start,
-      end: body.end,
+      days: workingDays,
+      start: time.start,
+      end: time.end,
     });
     const doctor = await this.doctorModel.findById(body.doctorID);
     doctor.shifts.push(shift);
@@ -46,11 +87,11 @@ export class ShiftService {
     return shift;
   }
 
-  async update(id: string, body: UpdateShiftDto) {
+  async update(id: ObjectId, body: UpdateShiftDto) {
     return this.shiftModel.findByIdAndUpdate(id, body);
   }
 
-  async delete(id: string) {
+  async delete(id: ObjectId) {
     return this.shiftModel.findByIdAndDelete(id);
   }
 }
