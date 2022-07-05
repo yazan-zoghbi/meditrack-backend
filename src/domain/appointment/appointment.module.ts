64141bd1be51a4f0579e463adfea61ac2ac92d4f
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-07-05 22:32:37 +0300

    [ADD] patient, doctor and services schma's imported

--- src/domain/appointment/appointment.module.ts
+++ src/domain/appointment/appointment.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Doctor, DoctorSchema } from '../doctor/schema/doctor.schema';
+import { Patient, PatientSchema } from '../patient/schema/patient.schema';
+import { Service, ServiceSchema } from '../service/service.schema';
 import { Appointment, appointmentSchema } from './appointment.schema';
 import { AppointmentService } from './appointment.service';
 
@@ -7,6 +10,9 @@ import { AppointmentService } from './appointment.service';
   imports: [
     MongooseModule.forFeature([
       { name: Appointment.name, schema: appointmentSchema },
+      { name: Patient.name, schema: PatientSchema },
+      { name: Doctor.name, schema: DoctorSchema },
+      { name: Service.name, schema: ServiceSchema },
     ]),
   ],
   providers: [AppointmentService],
