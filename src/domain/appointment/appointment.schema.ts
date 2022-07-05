b71f7d5da493d6b2d74e93b97ee17583285486f1
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-07-05 22:33:31 +0300

    [FIX] types & referance fixed

--- src/domain/appointment/appointment.schema.ts
+++ src/domain/appointment/appointment.schema.ts
@@ -1,5 +1,5 @@
 import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
-import mongoose, { Date } from 'mongoose';
+import mongoose from 'mongoose';
 import { DoctorCredential } from '../doctor/schema/doctor-credential.schema';
 import { PatientCredential } from '../patient/schema/patient-credential.schema';
 import { Service } from '../service/service.schema';
@@ -26,10 +26,14 @@ export class Appointment {
   active: boolean;
 
   @Prop({ required: true })
-  date: Date;
+  date: string;
 
-  @Prop({ required: true })
-  service: Service;
+  @Prop({
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Service',
+  })
+  serviceID: Service;
 }
 
 export const appointmentSchema = SchemaFactory.createForClass(Appointment);
