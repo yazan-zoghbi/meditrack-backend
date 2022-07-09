c428a4e064e63fa8a7d5b564595efe48a283cd00
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-07-09 05:13:05 +0300

    [ADD] create patient profile endpoint added

--- src/domain/patient/patient.controller.ts
+++ src/domain/patient/patient.controller.ts
@@ -3,6 +3,7 @@ import { PatientJwtGuard } from 'src/modules/jwt/jwt.guard';
 import { AppointmentService } from '../appointment/appointment.service';
 import { CreateAppointmentDto } from '../appointment/dto/create-appointment.dto';
 import { PatientCredentialDto } from './dto/patient-credential.dto';
+import { PatientProfileDto } from './dto/patient-profile.dto';
 import { PatientService } from './patient.service';
 
 @Controller('patient')
@@ -19,17 +20,23 @@ export class PatientController {
 
   @Post('login')
   async login(@Body() body: PatientCredentialDto) {
-    return this.patientService.login(body);
+    return await this.patientService.login(body);
   }
 
   @UseGuards(PatientJwtGuard)
-  @Post('Appointment/set')
+  @Post('profile')
+  async createProfile(@Body() body: PatientProfileDto) {
+    return await this.patientService.createProfile(body);
+  }
+
+  @UseGuards(PatientJwtGuard)
+  @Post('appointment/set')
   async setAppointment(@Body() body: CreateAppointmentDto) {
     return this.appointmentService.create(body);
   }
 
   @UseGuards(PatientJwtGuard)
-  @Get('appointment/:id')
+  @Get('appointment/all/:id')
   async getAppointments(@Param('id') id: string) {
     return this.appointmentService.getPatientAppointments(id);
   }
