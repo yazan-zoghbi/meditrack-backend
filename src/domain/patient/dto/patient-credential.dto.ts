54e41b6b3f7c8d3e23d4e4ea8712d01a68eaa68a
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-06-18 08:54:03 +0300

    [Chore]: class validator decorators added

--- src/domain/patient/dto/patient-credential.dto.ts
+++ src/domain/patient/dto/patient-credential.dto.ts
@@ -1,12 +1,18 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator';
 
 export class PatientCredentialDto {
   @ApiProperty()
+  @IsNotEmpty()
   username: string;
 
   @ApiProperty()
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 
   @ApiProperty()
+  @IsStrongPassword()
+  @IsNotEmpty()
   password: string;
 }
