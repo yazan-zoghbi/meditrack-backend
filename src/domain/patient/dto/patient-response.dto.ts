345d5a9b32bdadafa5cb75ce66457b0604d20572
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-06-18 13:22:30 +0300

    [Add]: login response dto added with examples for swagger

--- src/domain/patient/dto/patient-response.dto.ts
+++ src/domain/patient/dto/patient-response.dto.ts
@@ -17,8 +17,33 @@ export class PatientSignupResponseDto extends PatientResponseDto {
     },
   })
   data: {
+    id: string;
     username: string;
     email: string;
-    id: string;
+  };
+}
+
+export class PatientLoginResponseDto extends PatientResponseDto {
+  @ApiProperty({ example: 'You have successfully logged in.' })
+  message: string;
+
+  @ApiProperty({
+    example: {
+      patient: {
+        id: '648e84ff0aea4e7ba2773e4c',
+        username: 'johndoe',
+        email: 'john@test.com',
+      },
+      token:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImpvaG5kb2UyIiwicm9sZSI6InBhdGllbnQiLCJpYXQiOjE2ODcwODIwMTN9.H-w4f57IUdIfXsuLuwN3LFnrSc02yO4xj9yK8XwKkXY',
+    },
+  })
+  data: {
+    patient: {
+      id: string;
+      username: string;
+      email: string;
+    };
+    token: string;
   };
 }
