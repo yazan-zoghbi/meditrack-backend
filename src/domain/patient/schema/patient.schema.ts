0ea6027fbdd839611a5c62998ec777512a195bdb
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-06-19 06:51:07 +0300

    [Chore]: new properties added to patient schema: - birthDate: Date; - profession: string; - bloodType: BloodType; - chronicDiseases: ChronicDiseases;

--- src/domain/patient/schema/patient.schema.ts
+++ src/domain/patient/schema/patient.schema.ts
@@ -2,15 +2,20 @@ import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
 import { Appointment } from 'src/domain/appointment/appointment.schema';
 import { PatientCredential } from './patient-credential.schema';
+import { ChronicDiseases } from '../enums/chronic-diseases';
+import { BloodType } from '../enums/blood-type';
+import { ApiProperty } from '@nestjs/swagger';
 
 export type PatientDocument = Patient & Document;
 
 @Schema({ _id: false })
-export class Name {
+export class PatientName {
   @Prop({ required: true })
+  @ApiProperty()
   first: string;
 
   @Prop({ required: true })
+  @ApiProperty()
   last: string;
 }
 
@@ -19,8 +24,20 @@ export class Patient {
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'PatientCredential' })
   id: PatientCredential;
 
+  @Prop()
+  patientName: PatientName;
+
   @Prop({ required: true })
-  name: Name;
+  birthDate: Date;
+
+  @Prop()
+  profession: string;
+
+  @Prop({ required: true })
+  bloodType: BloodType;
+
+  @Prop()
+  chronicDiseases: ChronicDiseases;
 
   @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Appointment' })
   upcoming: Appointment[];
