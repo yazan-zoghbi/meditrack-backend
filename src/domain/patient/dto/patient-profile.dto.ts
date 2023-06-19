78f23ef502694c4afc89a9ade04e91247455fa93
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-06-19 06:47:46 +0300

    [Add]: new properties added to patient profile dto: - name : PatientName - birthDate : string - profession : string - bloodType: BloodType - chronicDiseases : ChronicDiseases

--- src/domain/patient/dto/patient-profile.dto.ts
+++ src/domain/patient/dto/patient-profile.dto.ts
@@ -1,12 +1,25 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { ChronicDiseases } from '../enums/chronic-diseases';
+import { BloodType } from '../enums/blood-type';
+import { PatientName } from '../schema/patient.schema';
 
 export class PatientProfileDto {
-  @ApiProperty()
-  id: string;
+  @ApiProperty({ type: PatientName })
+  name: PatientName;
 
-  @ApiProperty()
-  name: {
-    first: string;
-    last: string;
-  };
+  @ApiProperty({ example: '03/17/1987' })
+  birthDate: string = new Date().toLocaleString('en-US', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  });
+
+  @ApiPropertyOptional()
+  profession: string;
+
+  @ApiProperty({ enum: BloodType, enumName: 'Blood Type' })
+  bloodType: BloodType;
+
+  @ApiPropertyOptional({ enum: ChronicDiseases, enumName: 'Chronic Diseases' })
+  chronicDiseases?: ChronicDiseases;
 }
