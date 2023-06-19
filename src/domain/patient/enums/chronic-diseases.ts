85bcf9cc20c42c8c3658f2074938266ce40c2f1d
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2023-06-19 06:49:37 +0300

    [Add]: ChronicDiseases enum added

---
+++ src/domain/patient/enums/chronic-diseases.ts
@@ -0,0 +1,9 @@
+export enum ChronicDiseases {
+  Arthritis = 'Arthritis',
+  Asthma = 'Asthma',
+  Cancer = 'Cancer',
+  Cardiovascular_disease = 'Cardiovascular disease',
+  Chronic_obstructive_pulmonary_disease = 'Chronic obstructive pulmonary disease',
+  Diabetes = 'Diabetes',
+  Mental_health_conditions = 'Mental health conditions',
+}
